065a67f3729c44d9ffc4dbf4447d6fe1c42f6226
Sabari M <sabari@example.com>
2026-07-21 13:18:37 -0400

    Fix: rename 259_Select spec file and update template

--- tests/Template.spec.ts
+++ tests/Template.spec.ts
@@ -4,6 +4,6 @@ import { test, expect } from '@playwright/test';
 test('Verify our first TC', async ({ page }) => {
 
 
-
+    // await page.goto('https://the-internet.herokuapp.com/dropdown');
 
 });
